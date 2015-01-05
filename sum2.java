369d965db32b78233f8c517018c5122940d831ed
21111025 <21111025yi@tama.ac.jp>
2015-01-05 14:17:46 +0900

    sumの最新です

--- sum2.java
+++ sum2.java
@@ -1,27 +1,28 @@
 import java.util.Random;
 import java.io.*;
-
-
- class sum {
+ class sum{
         
-    public static void main(String args[])
-	{
+    public static void main(String args[]){
 		
 		try
 		{
 		    File file = new File("tensu.csv");
 		    FileReader fr = new FileReader(file);
 		    BufferedReader br = new BufferedReader(fr);
+			
 			String datas = br.readLine();
+			
+			
 			String[] str1Ary = datas.split(",");
-            int [] idatas=new int[str1Ary.length];			
+            int [] idatas=new int[str1Ary.length];	
+			int  num = 0;
 			for (int i=0; i<str1Ary.length; i++)
 			{
              idatas[i]=Integer.valueOf(str1Ary[i]).intValue();
 			
-			System.out.println(str1Ary[i]);
+			num = num + idatas[i];
 			}
-			
+			System.out.println(num);
 		    
 			
 		    
